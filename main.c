7da7316e7830023bde2a1d8269d8f4001788bb25
Giovanni Incammicia <giovanni.incammicia@gmail.com>
2015-10-26 10:35:30 +0100

    aggiunto algoritmo per convertire le coordinate IR in coordinate dello schermo

--- main.c
+++ main.c
@@ -41,6 +41,7 @@ GtkWidget *spinner;
 Display *dpy;
 Screen *s;
 int point, change;
+double matrix_A[9][9], matrix_x[9], matrix_res[20];;
 gboolean reset_point, freeze, stop_ir, calibrated;
 
 typedef struct point_s {
@@ -174,39 +175,8 @@ static int run_iface(struct xwii_iface *iface, struct xwii_event *event){
 	}
 }
 
-void calibration_thread(struct xwii_event *event_p){
-	int ret = run_iface(iface, event_p);
-	/*xwii_iface_unref(iface);
-	if (ret) {
-		print_error("Program failed; press any key to exit");
-		refresh();
-		timeout(-1);
-		getch();
-	}
-	endwin();*/
-}
-/*INTERFACCIA*/
-/* TODO fare un controllo sul massimo incremento possibile*/
-void change_distance_from_border(gboolean action){
-    int i;
-	if(action==SUM){
-        change++;
-    }
-    else if(action==SUBTRACT){
-        if(change>1) change--;
-    }
-    int new_distance = DEFAULT_DISTANCE_FROM_BORDER*change;
-    int new_right_distance = s->width - DEFAULT_DISTANCE_FROM_BORDER*change - LABEL_SIZE;
-    int new_bottom_distance = s->height - DEFAULT_DISTANCE_FROM_BORDER*change - LABEL_SIZE;
-	point_array[0].runtime_x = point_array[0].runtime_y = point_array[1].runtime_y = point_array[3].runtime_x = new_distance;
-	point_array[1].runtime_x = point_array[2].runtime_x = new_right_distance;
-	point_array[2].runtime_y = point_array[3].runtime_y = new_bottom_distance;
-	for(i=0;i<4;i++) gtk_layout_move((GtkLayout *)layout,point_array[i].label, point_array[i].runtime_x, point_array[i].runtime_y);
-}
-
-static gboolean reset(){
+static void reset(){
     point = 0;
-	change = 1;
 
 	gtk_widget_destroy((GtkWidget *) layout);
 	layout = (GObject *) gtk_layout_new(NULL, NULL);
@@ -224,7 +194,37 @@ static gboolean reset(){
 	}
 
 	gtk_widget_show_all((GtkWidget *) window);
-    return FALSE;
+}
+
+void calibration_thread(struct xwii_event *event_p){
+	int ret = run_iface(iface, event_p);
+	/*xwii_iface_unref(iface);
+	if (ret) {
+		print_error("Program failed; press any key to exit");
+		refresh();
+		timeout(-1);
+		getch();
+	}
+	endwin();*/
+}
+/*INTERFACCIA*/
+/* TODO fare un controllo sul massimo incremento possibile*/
+void change_distance_from_border(gboolean action){
+    int i;
+	reset();
+	if(action==SUM){
+        change++;
+    }
+    else if(action==SUBTRACT){
+        if(change>1) change--;
+    }
+    int new_distance = DEFAULT_DISTANCE_FROM_BORDER*change;
+    int new_right_distance = s->width - DEFAULT_DISTANCE_FROM_BORDER*change - LABEL_SIZE;
+    int new_bottom_distance = s->height - DEFAULT_DISTANCE_FROM_BORDER*change - LABEL_SIZE;
+	point_array[0].runtime_x = point_array[0].runtime_y = point_array[1].runtime_y = point_array[3].runtime_x = new_distance;
+	point_array[1].runtime_x = point_array[2].runtime_x = new_right_distance;
+	point_array[2].runtime_y = point_array[3].runtime_y = new_bottom_distance;
+	for(i=0;i<4;i++) gtk_layout_move((GtkLayout *)layout,point_array[i].label, point_array[i].runtime_x, point_array[i].runtime_y);
 }
 
 /* TODO controlla se è stato premuto ESC, funzione di callback del segnale associato*/
@@ -242,21 +242,113 @@ static gboolean key_event(GtkWidget *widget, GdkEventKey *event){
     return FALSE;
 }
 
+void diagonal(){
+	int i, j, k;
+	float temp=0;
+	for(i=0; i<8; i++){
+		if(matrix_A[i][i]==0){
+			for(j=0; j<8; j++){
+				if(j==i) continue;
+				if(matrix_A[j][i] !=0 && matrix_A[i][j]!=0){
+					for(k=0; k<8; k++){
+						temp = matrix_A[j][k];
+						matrix_A[j][k] = matrix_A[i][k];
+						matrix_A[i][k] = temp;
+					}
+					temp = matrix_x[j];
+					matrix_x[j] = matrix_x[i];
+					matrix_x[i] = temp;
+					break;
+				}
+			}
+		}
+	}
+}
+
+void post_calibration(){
+	int i, j, k;
+	printf("coordinate IR\n");
+	printf("%d %d\n",point_array[0].ir_x,point_array[0].ir_y );
+	printf("%d %d\n",point_array[1].ir_x,point_array[1].ir_y );
+	printf("%d %d\n",point_array[2].ir_x,point_array[2].ir_y );
+	printf("%d %d\n",point_array[3].ir_x,point_array[3].ir_y );
+	printf("coordinate schermo\n");
+	printf("%d %d\n",point_array[0].runtime_x,point_array[0].runtime_y );
+	printf("%d %d\n",point_array[1].runtime_x,point_array[1].runtime_y );
+	printf("%d %d\n",point_array[2].runtime_x,point_array[2].runtime_y );
+	printf("%d %d\n",point_array[3].runtime_x,point_array[3].runtime_y );
+	fflush(stdout);
+	calibrated = TRUE;
+	gtk_widget_destroy((GtkWidget *)window);
+
+	//creazione matrice
+	for(i=0;i<8;i=i+2){
+		matrix_A[i][0] = point_array[i/2].ir_x;
+		matrix_A[i][1] = point_array[i/2].ir_y;
+		matrix_A[i][2] = 1;
+		matrix_A[i][3] = matrix_A[i][4] = matrix_A[i][5] = 0;
+		matrix_A[i][6] = -(point_array[i/2].runtime_x * point_array[i/2].ir_x);
+		matrix_A[i][7] = -(point_array[i/2].runtime_x * point_array[i/2].ir_y);
+
+		matrix_A[i+1][0] = matrix_A[i+1][1] = matrix_A[i+1][2] = 0;
+		matrix_A[i+1][3] = point_array[i/2].ir_x;
+		matrix_A[i+1][4] = point_array[i/2].ir_y;
+		matrix_A[i+1][5] = 1;
+		matrix_A[i+1][6] = -(point_array[i/2].runtime_y * point_array[i/2].ir_x);
+		matrix_A[i+1][7] = -(point_array[i/2].runtime_y * point_array[i/2].ir_y);
+
+		matrix_x[i] = point_array[i/2].runtime_x;
+		matrix_x[i+1] = point_array[i/2].runtime_y;
+	}
+	printf("matrice A | matrice x\n");
+	for(i=0;i<8;i++){
+		for(j=0;j<8;j++){
+			printf("%d ", (int)matrix_A[i][j]);
+		}
+		printf("| %d", (int)matrix_x[i]);
+		printf("\n");
+	}
+	/*----------------------------secondo metodo----------------------------------*/
+	/*http://www.alexeypetrov.narod.ru/Eng/C/gauss_about.html*/
+	diagonal();
+   //process rows
+	for(k=0; k<8; k++){
+		for(i=k+1; i<8; i++){
+			if(matrix_A[k][k]==0){
+				printf("\nSolution is not exist.\n");
+				return;
+			}
+			float M = matrix_A[i][k] / matrix_A[k][k];
+			for(j=k; j<8; j++)
+				matrix_A[i][j] -= M * matrix_A[k][j];
+			matrix_x[i] -= M*matrix_x[k];
+		}
+	}
+	for(i=8-1; i>=0; i--){
+		float s = 0;
+		for(j = i; j<8; j++)
+			s = s+matrix_A[i][j]*matrix_res[j];
+		matrix_res[i] = (matrix_x[i] - s) / matrix_A[i][i];
+	}
+	printf("Result:\n");
+	for(i=0; i<8; i++){
+		printf("X%d = %lf\n", i, matrix_res[i]);
+	}
+	printf("\nadesso stampo le coordinate schermo del primo punto\n");
+	float new_x,new_y;
+	new_x = ((matrix_res[0]*point_array[0].ir_x) + (matrix_res[1]*point_array[0].ir_y) + matrix_res[2]) /
+			((matrix_res[6]*point_array[0].ir_x) + (matrix_res[7]*point_array[0].ir_y) + 1);
+	new_y = ((matrix_res[3]*point_array[0].ir_x) + (matrix_res[4]*point_array[0].ir_y) + matrix_res[5]) /
+			((matrix_res[6]*point_array[0].ir_x) + (matrix_res[7]*point_array[0].ir_y) + 1);
+	printf("\n%f %f\n", new_x, new_y);
+	fflush(stdout);
+}
+
 gboolean post_sleep_calibration(){
 	if(!reset_point) gtk_widget_destroy((GtkWidget *) spinner);
 	else reset();
 	stop_ir = FALSE;
-
-	if(point==4){ // TODO eliminare
-		printf("%d %d\n",point_array[0].ir_x,point_array[0].ir_y );
-		printf("%d %d\n",point_array[1].ir_x,point_array[1].ir_y );
-		printf("%d %d\n",point_array[2].ir_x,point_array[2].ir_y );
-		printf("%d %d\n",point_array[3].ir_x,point_array[3].ir_y );
-		fflush(stdout);
-		calibrated = TRUE;
-		gtk_widget_destroy((GtkWidget *)window);
-	}
-
+	if(point==4) post_calibration();
 	return FALSE;
 }
 
@@ -282,15 +374,22 @@ void point_f(struct xwii_event *event){
 int main(int argc, char **argv){
 	struct xwii_event event;
 	reset_point=FALSE;
+	change = 1;
 
 	void sig_handler(int signo){
-		if (signo == SIGUSR1){ //IR
-			if(!calibrated && !freeze && xwii_event_ir_is_valid(&event.v.abs[0])) point_f(&event);
+		if (!calibrated && signo==SIGUSR1){ //IR
+			if(!freeze && xwii_event_ir_is_valid(&event.v.abs[0])) point_f(&event);
 			//ir_show(&event);
 		}
-		else if(signo==SIGUSR2){ //no IR
-			if(!calibrated && stop_ir) reset_point = TRUE;
+		else if(!calibrated && signo==SIGUSR2){ //no IR
+			if(stop_ir) reset_point = TRUE;
 			else freeze=FALSE;
+		}
+		else if(calibrated && signo==SIGUSR1){
+
+		}
+		else if(calibrated && signo==SIGUSR2){
+
 		}
 	}
 
