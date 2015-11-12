f6be5d20c0d08625721b5fffd7736b6d582e9bc3
Giovanni Incammicia <giovanni.incammicia@gmail.com>
2015-11-12 13:13:01 +0100

    conversion gtk+ --> qt

--- src/calibration.cpp
+++ src/calibration.cpp
@@ -1,27 +1,25 @@
-#include <errno.h>
-#include <string.h>
-#include <unistd.h>
+#include <QTimer>
+#include <QObject>
 #include <poll.h>
-#include <gtk/gtk.h>
-#include <X11/Xlib.h>
-#include <X11/extensions/XTest.h>
+#include <errno.h>
+#include <unistd.h>
 #include <pthread.h>
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
 #include "../include/xwiimote.h"
 #include "../include/main.h"
+#include <X11/Xlib.h>
+#include <X11/extensions/XTest.h>
 
 extern struct xwii_iface *iface;
 extern pthread_t main_tid;
-
-extern GObject *layout, *window;
-extern GtkBuilder *builder;
-extern GtkWidget *spinner;
+extern CalibrationWindow *window;
+extern int point, change;
 extern Display *dpy;
 extern Screen *s;
-extern int point, change;
 extern double matrix_A[9][9], matrix_x[9], matrix_res[20];
-extern gboolean reset_point, freeze, stop_ir, calibrated;
-
-point_s point_array[4];
+extern int reset_point, freeze, stop_ir, calibrated;
 
 static int run_iface(struct xwii_iface *iface, struct xwii_event *event){
 	int ret=0, fds_num;
@@ -76,8 +74,7 @@ static int run_iface(struct xwii_iface *iface, struct xwii_event *event){
 									((matrix_res[6]*event->v.abs[0].x) + (matrix_res[7]*event->v.abs[0].y) + 1);
 
 							XTestFakeMotionEvent (dpy, 0, new_x, new_y, CurrentTime);
-			  				XSync(dpy, 0);
-
+							XSync(dpy, 0);
 						}
 					}
 					break;
@@ -87,8 +84,8 @@ static int run_iface(struct xwii_iface *iface, struct xwii_event *event){
 	}
 }
 
-void calibration_thread(struct xwii_event *event_p){
-	int ret = run_iface(iface, event_p);
+void *calibration_thread(void *event_p){
+	int ret = run_iface(iface, (struct xwii_event *)event_p);
 	xwii_iface_unref(iface);
 	if (ret) {
 		printf("Program failed");
@@ -97,7 +94,7 @@ void calibration_thread(struct xwii_event *event_p){
 }
 
 /* TODO make a control about maximum increasing*/
-void change_distance_from_border(gboolean action){
+void change_distance_from_border(int action){
     int i;
 	reset();
 	if(action==SUM){
@@ -109,10 +106,26 @@ void change_distance_from_border(gboolean action){
     int new_distance = DEFAULT_DISTANCE_FROM_BORDER*change;
     int new_right_distance = s->width - DEFAULT_DISTANCE_FROM_BORDER*change - LABEL_SIZE;
     int new_bottom_distance = s->height - DEFAULT_DISTANCE_FROM_BORDER*change - LABEL_SIZE;
-	point_array[0].runtime_x = point_array[0].runtime_y = point_array[1].runtime_y = point_array[3].runtime_x = new_distance;
-	point_array[1].runtime_x = point_array[2].runtime_x = new_right_distance;
-	point_array[2].runtime_y = point_array[3].runtime_y = new_bottom_distance;
-	for(i=0;i<4;i++) gtk_layout_move((GtkLayout *)layout,point_array[i].label, point_array[i].runtime_x, point_array[i].runtime_y);
+	window->point_array[0].runtime_x = window->point_array[0].runtime_y = window->point_array[1].runtime_y = window->point_array[3].runtime_x = new_distance;
+	window->point_array[1].runtime_x = window->point_array[2].runtime_x = new_right_distance;
+	window->point_array[2].runtime_y = window->point_array[3].runtime_y = new_bottom_distance;
+	for(i=0;i<4;i++) window->point_array[i].label->setGeometry(window->point_array[i].runtime_x, window->point_array[i].runtime_y, LABEL_SIZE, LABEL_SIZE);
+}
+
+void point_f(struct xwii_event *event){
+	freeze = TRUE;
+	stop_ir = TRUE;
+	reset_point=FALSE; //if after timeout this variable is set to TRUE, reset()
+
+	if(point<4){
+		//put spinner here
+		window->point_array[point].label->hide();
+
+		window->point_array[point].ir_x = event->v.abs[0].x;
+		window->point_array[point].ir_y = event->v.abs[0].y;
+		point++;
+		window->setTimer(); //sets a 1'' timeout, then calls post_sleep_calibration function
+	}
 }
 
 static void diagonal(){
@@ -138,29 +151,28 @@ static void diagonal(){
 	}
 }
 
-static void post_calibration(){
+void post_calibration(){
 	int i, j, k;
 	calibrated = TRUE;
-	gtk_widget_destroy((GtkWidget *)window);
 
 	//matrices creation
 	for(i=0;i<8;i=i+2){
-		matrix_A[i][0] = point_array[i/2].ir_x;
-		matrix_A[i][1] = point_array[i/2].ir_y;
+		matrix_A[i][0] = window->point_array[i/2].ir_x;
+		matrix_A[i][1] = window->point_array[i/2].ir_y;
 		matrix_A[i][2] = 1;
 		matrix_A[i][3] = matrix_A[i][4] = matrix_A[i][5] = 0;
-		matrix_A[i][6] = -(point_array[i/2].runtime_x * point_array[i/2].ir_x);
-		matrix_A[i][7] = -(point_array[i/2].runtime_x * point_array[i/2].ir_y);
+		matrix_A[i][6] = -(window->point_array[i/2].runtime_x * window->point_array[i/2].ir_x);
+		matrix_A[i][7] = -(window->point_array[i/2].runtime_x * window->point_array[i/2].ir_y);
 
 		matrix_A[i+1][0] = matrix_A[i+1][1] = matrix_A[i+1][2] = 0;
-		matrix_A[i+1][3] = point_array[i/2].ir_x;
-		matrix_A[i+1][4] = point_array[i/2].ir_y;
+		matrix_A[i+1][3] = window->point_array[i/2].ir_x;
+		matrix_A[i+1][4] = window->point_array[i/2].ir_y;
 		matrix_A[i+1][5] = 1;
-		matrix_A[i+1][6] = -(point_array[i/2].runtime_y * point_array[i/2].ir_x);
-		matrix_A[i+1][7] = -(point_array[i/2].runtime_y * point_array[i/2].ir_y);
+		matrix_A[i+1][6] = -(window->point_array[i/2].runtime_y * window->point_array[i/2].ir_x);
+		matrix_A[i+1][7] = -(window->point_array[i/2].runtime_y * window->point_array[i/2].ir_y);
 
-		matrix_x[i] = point_array[i/2].runtime_x;
-		matrix_x[i+1] = point_array[i/2].runtime_y;
+		matrix_x[i] = window->point_array[i/2].runtime_x;
+		matrix_x[i+1] = window->point_array[i/2].runtime_y;
 	}
 
 	/*http://www.alexeypetrov.narod.ru/Eng/C/gauss_about.html*/
@@ -184,32 +196,5 @@ static void post_calibration(){
 		for(j = i; j<8; j++)
 			s = s+matrix_A[i][j]*matrix_res[j];
 		matrix_res[i] = (matrix_x[i] - s) / matrix_A[i][i];
-	}
-}
-
-static gboolean post_sleep_calibration(){
-	if(!reset_point) gtk_widget_destroy((GtkWidget *) spinner);
-	else reset();
-	stop_ir = FALSE;
-	if(point==4) post_calibration();
-	return FALSE;
-}
-
-void point_f(struct xwii_event *event){
-	freeze = TRUE;
-	stop_ir = TRUE;
-	reset_point=FALSE; //if after timeout this variable is set to TRUE, reset()
-	if(point<4){
-		spinner = gtk_spinner_new();
-		gtk_spinner_start (GTK_SPINNER (spinner));
-		gtk_widget_set_size_request(spinner,40,40);
-		gtk_layout_put((GtkLayout *)layout,spinner,(gint) point_array[point].runtime_x-10,point_array[point].runtime_y-10); /*centering spinners*/
-		gtk_widget_destroy(GTK_WIDGET(point_array[point].label));
-		gtk_widget_show(spinner);
-
-		point_array[point].ir_x = event->v.abs[0].x;
-		point_array[point].ir_y = event->v.abs[0].y;
-		point++;
-		g_timeout_add(1000, (GSourceFunc)post_sleep_calibration, NULL);
 	}
 }
